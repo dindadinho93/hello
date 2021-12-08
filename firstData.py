33ea2f3475c4a6669866c4e747bc0be6a23a96fb
dinda <dindadinho93@gmail.com>
2021-12-08 20:03:36 +0700

    first

---
+++ firstData.py
@@ -0,0 +1,26 @@
+import matplotlib_inline
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import gensim as gensim
+import nltk
+import seaborn as sns
+import sklearn as sck
+import keras
+import tensorflow as tf
+import re
+import tensorflow_hub as hub
+import torch
+
+a = 121
+b = 12
+print(a+b)
+
+if a == b:
+    print("ok")
+
+else:
+    print("not")
+
+
+model = sck.linear_model.LinearRegression()
